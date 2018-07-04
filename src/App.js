1aa520c217771aff76b650a6ea2747d3c5bab3bb
gosho-kazuya <ketsume0211@gmail.com>
2018-07-04 23:00:34 +0900

    complete convert to html

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
-import React, { Component } from 'react';
-
-import {Editor, EditorState} from 'draft-js';
-
-import { convertFromRaw, convertToRaw } from 'draft-js';
-
-import {RichUtils} from 'draft-js';
+import React, {Component} from 'react';
+import {stateToHTML} from 'draft-js-export-html';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import {convertFromRaw, convertToRaw} from 'draft-js';
 
 class MyEditor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {editorState: EditorState.createEmpty()};
-        this.onChange = (editorState) => this.setState({editorState});
-        this.handleKeyCommand = this.handleKeyCommand.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = editorState => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
 
-    handleKeyCommand(command, editorState) {
-        const newState = RichUtils.handleKeyCommand(editorState, command);
-        if (newState) {
-            this.onChange(newState);
-            return 'handled';
-        }
-        return 'not-handled';
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
     }
+    return 'not-handled';
+  }
 
-    bold = () => {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-    }
+  bold = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  };
 
-    debug = () => {
-        console.log(convertToRaw(this.state.editorState.getCurrentContent()))
-        const self = this
-        // debugger
-    }
+  debug = () => {
+    console.log(convertToRaw(this.state.editorState.getCurrentContent()));
+    const self = this;
+    stateToHTML(this.state.editorState.getCurrentContent());
+    debugger;
+  };
 
-    render() {
-        return (
-            <div style={{ width: '80%', margin: 'auto' }}>
-                <div style={{}}>
-                    <button onClick={this.debug}>Debug</button>
-                    <button onClick={this.bold}>Bold</button>
-                </div>
+  render() {
+    const {editorState} = this.state;
 
-                <div style={{ marginTop: 50, border: 'solid 1px #ccc' }}>
-                    <Editor
-                        editorState={this.state.editorState}
-                        handleKeyCommand={this.handleKeyCommand}
-                        onChange={this.onChange}
-                    />
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div style={{width: '80%', margin: 'auto'}}>
+        <div style={{}}>
+          <button onClick={this.debug}>Debug</button>
+          <button onClick={this.bold}>Bold</button>
+        </div>
+
+        <div style={{marginTop: 50, border: 'solid 1px #ccc'}}>
+          <Editor
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange}
+          />
+        </div>
+        <div style={{marginTop: 50, border: 'solid 1px #ccc'}}>
+          <textarea
+            value={stateToHTML(this.state.editorState.getCurrentContent())}
+            readOnly
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MyEditor
+export default MyEditor;
