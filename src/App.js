e58bc12abab7b95f410bca2316e051b1b2d19f73
gosho-kazuya <ketsume0211@gmail.com>
2018-07-04 22:49:19 +0900

    create editor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import {Editor, EditorState} from 'draft-js';
+
+import { convertFromRaw, convertToRaw } from 'draft-js';
+
+import {RichUtils} from 'draft-js';
+
+class MyEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    }
+
+    handleKeyCommand(command, editorState) {
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+
+    bold = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
+
+    debug = () => {
+        console.log(convertToRaw(this.state.editorState.getCurrentContent()))
+        const self = this
+        // debugger
+    }
+
+    render() {
+        return (
+            <div style={{ width: '80%', margin: 'auto' }}>
+                <div style={{}}>
+                    <button onClick={this.debug}>Debug</button>
+                    <button onClick={this.bold}>Bold</button>
+                </div>
+
+                <div style={{ marginTop: 50, border: 'solid 1px #ccc' }}>
+                    <Editor
+                        editorState={this.state.editorState}
+                        handleKeyCommand={this.handleKeyCommand}
+                        onChange={this.onChange}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default MyEditor
